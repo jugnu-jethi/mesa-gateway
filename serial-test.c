ec55f6c1ae75930420f19fa27a393fcacbfe20fc
Jugnu Jethi <jugnu.jethi@mimos.my>
2015-02-06 17:05:44 +0800

    Included minor changes from declaring defines.

--- serial-test.c
+++ serial-test.c
@@ -9,13 +9,15 @@
 #define FALSE ( 0 )
 #define TRUE ( unsigned )( !FALSE )
 
+#define COLLECTOR_TTY "/dev/ttyUSB0"
+
 
 
 int main( void ){
   
   int collectorfd = -1;
   
-  collectorfd = open( "/dev/ttyUSB0", O_RDWR | O_NOCTTY );
+  collectorfd = open( COLLECTOR_TTY, O_RDWR | O_NOCTTY );
   if( collectorfd < 0 ){
     perror( "open-collector" ); 
     exit( EXIT_FAILURE );
